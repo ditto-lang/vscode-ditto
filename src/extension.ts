608842cfb0796b1951abe99846bb228c4edf4a0b
Jordan Mackie <jmackie@protonmail.com>
2022-04-04 13:21:53 +0100

    Plug the ditto lsp into a VS Code extension ✨

---
+++ src/extension.ts
@@ -0,0 +1,51 @@
+import * as vscode from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+function startDittoLanguageServer(): LanguageClient {
+  const config = vscode.workspace.getConfiguration("ditto");
+  const serverOptions: ServerOptions = {
+    command: config.path,
+    args: ["lsp"],
+    transport: TransportKind.stdio,
+    debug: {
+      command: config.path,
+      args: ["lsp"],
+      transport: TransportKind.stdio,
+      options: {
+        env: {
+          // TODO make this configurable?
+          DITTO_LOG_DIR: ".ditto/logs",
+        },
+      },
+    },
+  };
+  const clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "ditto" }],
+  };
+  client = new LanguageClient(
+    "dittoLanguageServer",
+    "Ditto Language Server",
+    serverOptions,
+    clientOptions
+  );
+  client.start();
+  return client;
+}
+
+let client: LanguageClient;
+
+export function activate(_context: vscode.ExtensionContext) {
+  client = startDittoLanguageServer();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+  if (!client) {
+    return undefined;
+  }
+  return client.stop();
+}
